f611c53d2631c4de7aa03b96c5bbde9efd0eeee9
Joey PLEVY <jplevy@e2r2p1.42.fr>
2018-10-22 20:49:12 +0200

    ok

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: jplevy <jplevy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 15:39:03 by jplevy            #+#    #+#             */
-/*   Updated: 2018/10/21 20:18:14 by jplevy           ###   ########.fr       */
+/*   Updated: 2018/10/22 20:21:36 by jplevy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,64 @@
 # define USAGE "usage: ft_ssl command [command opts] [command args]\n"
 # define FMT "ft_ssl: Error: '%s' is an invalid command.\nStandard \
 commands:\n\nMessage Digest commands:\nmd5\nsha256\n\nCipher commands:\n"
+# define NB_FLAG 4
+# define NB_HASH 2
 
 # include <libft.h>
+# include <unistd.h>
+# include <fcntl.h>
+/*
+ -s     string Print a checksum of the given string. | char *
 
-typedef enum    e_prog{
-    MD5,
-    SHA256
-}               t_prog;
+-p      Echo stdin to stdout and append the checksum to stdout. -p | fd
 
-static const void (*hash_funcs[2])(int,char **)  = {
-    &ft_md5,
-    &ft_sha256,
+nothing open file | fd
+
+-q      Quiet mode - only the checksum is printed out.  Overrides the -r option.
+
+-r      Reverses the format of the output.  This helps with visual diffs.  Does nothing when combined with the -ptx options.
+*/
+typedef enum    e_flags{
+    NONE, 
+    QUIET,
+    REVERSE
+}               t_flags;
+
+typedef struct  s_args {
+    int         fd;
+    t_flags     flag;
+    char        last_f;
+    char        *str;
+    char        *next;
+}               t_args;
+
+typedef struct  s_funcs {
+    char        *ref;
+    void        (*func_call)(t_args *);
+}               t_funcs;
+
+typedef struct  s_flag_funcs {
+    char        *ref;
+    void        (*func_call)(t_args *, void (*)(t_args *));
+}               t_flag_funcs;
+
+void    ft_sha256(t_args *args);
+void    ft_md5(t_args *args);
+void    ft_string(t_args *args, void (*func_call)(t_args *));
+void    ft_stdin(t_args *args, void (*func_call)(t_args *));
+void    ft_set_q(t_args *args, void (*func_call)(t_args *));
+void    ft_set_r(t_args *args, void (*func_call)(t_args *));
+
+static const t_funcs funcs_calls[NB_HASH] = {
+    {"md5", &ft_md5},
+    {"sha256", &ft_sha256},
 };
 
-static const char *funcs_call[2]  = {
-    "md5",
-    "sha256",
+static const t_flag_funcs flag_calls[NB_FLAG]  = {
+    {"s", &ft_string},
+    {"p", &ft_stdin},
+    {"q", &ft_set_q},
+    {"r", &ft_set_r}
 };
 
 #endif
--- srcs/ft_ssl.c
+++ srcs/ft_ssl.c
@@ -5,46 +5,153 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jplevy <jplevy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/21 18:58:53 by jplevy            #+#    #+#             */
-/*   Updated: 2018/10/21 20:17:16 by jplevy           ###   ########.fr       */
+/*   Created: 2018/10/22 16:14:23 by jplevy            #+#    #+#             */
+/*   Updated: 2018/10/22 20:47:38 by jplevy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ssl.h>
 
-void    ft_md5(int ac, char **av)
+void	ft_md5(t_args *args)
 {
-    if (ac || av)
-        ft_putstr("ft_md5 call\n");
+	__uint32_t	buff[16];
+	int			i;
+
+	if (args->fd >= 0)
+	{
+		ft_bzero(buff, 16 * sizeof(__uint32_t));
+		while (read(args->fd, buff, 16 * sizeof(__uint32_t)) > 0)
+		{
+			i = -1;
+			while (++i < 16)
+				ft_printf("%x\n", buff[i]);
+			ft_bzero(buff, 16 * sizeof(__uint32_t));
+		}
+		// ft_printf("should read fd %d\n", args->fd);
+	}
+	else
+		ft_putendl(args->str);
 }
 
-void    ft_sha256(int ac, char **av)
+void	ft_sha256(t_args *args)
 {
-    if (ac || av)
-        ft_putstr("ft_md5 call\n");
+	if (args)
+		ft_putstr("ft_sha256 call\n");
 }
 
-t_prog  ft_get_prog(char *input)
+t_args	ft_init_args(void)
 {
+	t_args	ret;
 
+	ret.fd = -1;
+	ret.flag = NONE;
+	ret.str = NULL;
+	return (ret);
 }
 
-int     ft_ssl(int ac, char **av)
+void    ft_string(t_args *args, void (*func_call)(t_args *))
 {
-    t_prog  prog;
-
-    prog = ft_get_prog(av[0]);
-    hash_funcs[prog](ac, av);
-    // if ((ft_strcmp(av[0], "md5")) || (ft_strcmp(av[0], "sha256")))
-    //     return (0);
-    return (1);
+	args->fd = -1;
+	ft_putendl("string call");
+	if (args->str)
+		func_call(args);
+	else
+		ft_putendl("option requires an argument -- s");
+}
+void    ft_stdin(t_args *args, void (*func_call)(t_args *))
+{
+	ft_putendl("stdin call");
+	args->fd = 0;
+	func_call(args);
+}
+void    ft_set_q(t_args *args, void (*func_call)(t_args *))
+{
+	ft_putendl("set q");
+	if (func_call)
+		args->flag |= QUIET;
+}
+void    ft_set_r(t_args *args, void (*func_call)(t_args *))
+{
+	ft_putendl("set r");
+	if (func_call)
+		args->flag |= REVERSE;
 }
 
-int     main(int ac, char **av)
+void	ft_get_flags(char *str, t_args *args, void (*func_call)(t_args *))
 {
-    if (ac < 2)
-        ft_putstr(USAGE);
-    else if (!(ft_ssl(ac - 1, av + 1)))
-        ft_printf(FMT, av[1]);
-    return (0);
+	int		i;
+	int		found;
+
+	while (*str)
+	{
+		args->str = (*(str + 1)) ? str + 1 : args->next;
+		i = -1;
+		found = 0;
+		while (++i < NB_FLAG)
+		{
+			if (*str == (flag_calls[i]).ref[0])
+			{
+				args->last_f = *str;
+				found = 1;
+				(flag_calls[i]).func_call(args, func_call);
+				if (*str == 's')
+					return;
+			}
+		}
+		if (!found)
+			ft_printf("illegal option -- %c\n", *str);
+		str++;
+	}
+}
+
+
+int	 	ft_ssl(int ac, char **av)
+{
+	int		i;
+	t_args	args;
+
+	i = -1;
+	args = ft_init_args();
+	while (++i < NB_HASH)
+	{
+		if (ft_strcmp(av[0], (funcs_calls[i]).ref) == 0)
+		{
+			if (ac == 1)
+			{
+				args.fd = 0;
+				(funcs_calls[i]).func_call(&args);
+			}
+			while (--ac > 0 && ++av)
+			{
+				args.next = (ac > 0) ? *(av + 1) : NULL;
+				if ((*av)[0] == '-')
+				{
+					ft_get_flags((*av) + 1, &(args), (funcs_calls[i]).func_call);
+					if (args.next == args.str && args.last_f == 's' && --ac)
+						++av;
+				}
+				else if ((args.fd = open(*av, O_RDONLY)) > 0)
+				{
+					(funcs_calls[i]).func_call(&args);
+					close(args.fd);
+				}
+				else
+					ft_printf("%s :No such file or directory", *av);
+				// av++;
+				// ft_putstr(*av);
+				// (funcs_calls[i]).func_call(ac - 1, av);
+			}
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	 main(int ac, char **av)
+{
+	if (ac < 2)
+		ft_putstr(USAGE);
+	else if (!(ft_ssl(ac - 1, av + 1)))
+		ft_printf(FMT, av[1]);
+	return (0);
 }
